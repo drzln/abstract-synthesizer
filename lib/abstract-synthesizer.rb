325cafd012bd06343b49518187701e1b03860003
luis <luisfelipemourapereira@protonmail.com>
2023-11-24 16:51:14 -0500

    add proper gem dep

--- lib/abstract-synthesizer.rb
+++ lib/abstract-synthesizer.rb
@@ -6,7 +6,7 @@ require_relative %(./abstract-synthesizer/primitives/bury)
 class AbstractSynthesizer
   include Bury
 
-  attr_reader :translation
+  attr_reader :translation, :data
 
   def initialize(name: nil) # rubocop:disable Lint/UnusedMethodArgument
     @translation = {
@@ -14,6 +14,11 @@ class AbstractSynthesizer
       template: {},
       context: nil
     }
+  end
+
+  def load_synthesis_data(data)
+    @data = data
+    @data
   end
 
   def synthesis
